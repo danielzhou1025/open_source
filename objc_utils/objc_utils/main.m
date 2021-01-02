6319b821b2d611a349d88c1c82e0ed4f0308758a
daniel <danielzhou1025@163.com>
2021-01-02 20:32:32 +0800

    add objc utils project

---
+++ objc_utils/objc_utils/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  objc_utils
+//
+//  Created by daniel on 2021/1/2.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
